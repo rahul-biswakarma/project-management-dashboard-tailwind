380dde6b55a3b28347f8bda8016f0fb69bf72ef6
Rahul Biswakarma <rahul.id39@gmail.com>
2022-12-29 19:18:51 +0530

    Sidenav Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#23242b",
+        "bg-2": "#2c3034",
+        "bg-3": "#7f7f7f",
+        "light-border-color": "#7f7f7f",
+        "dark-border-color": "#15161a",
+        "text-1": "#c0bfc1",
+        "text-2": "#838383",
+        purple: "#6c50a0",
+        "light-red": "#f66673",
+        "icons-color": "#7f7f7f",
+        "button-color-1": "#34383c",
+      },
+    },
+    spacing: {
+      "d-pad": "1.5rem",
+    },
   },
   plugins: [],
 };
