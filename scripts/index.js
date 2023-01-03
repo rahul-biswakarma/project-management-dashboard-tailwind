61828b431af346c1c66db6e30ab2bf8582ca41bd
Rahul Biswakarma <rahul.id39@gmail.com>
2023-01-03 12:30:25 +0530

    Added Ham Icon with functionalities

--- scripts/index.js
+++ scripts/index.js
@@ -10,10 +10,14 @@ function collapseSideNav() {
       element.style.padding = "10px";
       element.style.justifyContent = "center";
     });
+    document.getElementById("sidenav-header").style.padding = "1.5rem";
     document.getElementById("sidenav-controller").style.transform =
       "rotate(180deg)";
     sideNavExpanded = false;
   } else {
+    if (window.innerWidth <= 650) {
+      document.getElementById("sidenav-container").style.position = "fixed";
+    }
     document.querySelector("body").style.gridTemplateColumns = "250px auto";
     document.querySelectorAll(".sidenav-text").forEach((element) => {
       element.style.display = "block";
@@ -22,8 +26,25 @@ function collapseSideNav() {
       element.style.padding = "0.5rem 2rem";
       element.style.justifyContent = "flex-start";
     });
+    document.getElementById("sidenav-header").style.padding = "1.5rem";
     document.getElementById("sidenav-controller").style.transform =
       "rotate(0deg)";
     sideNavExpanded = true;
   }
+}
+
+// window.onresize = function () {
+//   if (window.innerWidth <= 850 && sideNavExpanded) {
+//     collapseSideNav();
+//   } else if (window.innerWidth > 850 && !sideNavExpanded) {
+//     collapseSideNav();
+//   }
+// };
+
+function openSideNav() {
+  document.getElementById("sidenav-container").style.left = "0px";
+}
+
+function closeSideNav() {
+  document.getElementById("sidenav-container").style.left = "-100%";
 }
